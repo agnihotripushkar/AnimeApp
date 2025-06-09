0357b2c57c64d9d438b87b86c3f65db3761e67b1
Pushkar Aagnihotri <agnihotripush@gmail.com>
2025-06-09 00:49:25 -0500

    :bug: import bug fix

--- app/src/main/java/com/devpush/animeapp/features/auth/data/repository/AuthRepositoryImpl.kt
+++ app/src/main/java/com/devpush/animeapp/features/auth/data/repository/AuthRepositoryImpl.kt
@@ -1,7 +1,6 @@
 package com.devpush.animeapp.features.auth.data.repository
 
 import com.devpush.animeapp.features.auth.domain.repository.AuthRepository
-import com.devpush.animeapp.core.network.KitsuApi
 import io.ktor.client.HttpClient
 
 class AuthRepositoryImpl(val client: HttpClient) : AuthRepository {
